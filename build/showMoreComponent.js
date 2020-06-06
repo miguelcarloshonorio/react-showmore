574734eea8b54f44152ddcd37e9af4a4fe2c8137
Miguel Carlos Honorio <miguel@prosulti.com.br>
2020-06-06 17:23:10 -0300

    React remove router-native

--- build/showMoreComponent.js
+++ build/showMoreComponent.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-native';
+import { Link } from 'react-router-dom';
 export default function ShowMore(props) {
     var linkStyles = {
         color: '#0288d1',
